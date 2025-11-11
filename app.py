a852a0bc88d83bc70d7f9cddacebc714cc38de5f
Kiamehr <kiamehrmoradii@gmail.com>
2025-11-11 11:41:00 +0330

    improved version

--- app.py
+++ app.py
@@ -1,47 +1,52 @@
-from flask import Flask
-from flask import render_template
-from flask import request
-from flask import redirect
-from flask import url_for
-
+from flask import Flask, render_template, url_for, request, redirect
+from random import randint
 
 app = Flask(__name__)
 
-tasks = ["Buy groceries", "Being The Best at Coding", "Doing college HomeWork"]
+tasks = [
+    {
+        "id": 1,
+        "name": "Be the Best At Coding",
+        # checkbox is checked or not
+        "checked": False,
+    },
+    {"id": 1, "name": "Hit The Gym", "checked": True},
+]
 
 
 @app.route("/")
-# redering the whole web
-def index():
+@app.route("/home", methods=["GET", "POST"])  # methods are (get) and (post)
+def home():
+    if request.method == "POST":
+        todo_name = request.form["todo_name"]
+        cur_id = randint(1, 1000)
+        tasks.append({
+            "id" : cur_id,
+            "name" : todo_name,
+            "checked" : False,
+        })
+
     return render_template("index.html", tasks=tasks)
 
+# <int:todo_id> = its gonna get an integer thats called todo_id \
+# so the checked is for that
+@app.route("/checked<int:todo_id>", methods=["POST"])
+def checked_todo(todo_id):
+    for task in tasks:
+        if task['id'] == todo_id:
+            task["checked"] = not task["checked"]
+            break
+    
+    return redirect(url_for("home"))
+    
 
-@app.route("/add", methods=["POST"])
-# adding a task
-def add_task():
-    new_task = request.form.get("newTask")
-    if new_task:
-        tasks.append(new_task)
-    return redirect(url_for("index"))
-
-
-# adding a completed task
-@app.route("/complete", methods=["POST"])
-def complete_tasks():
-    completed_tasks = request.form.getlist('taskCheckbox')
-    for index in map(int, completed_tasks):
-        if 1 <= index <= len(tasks):
-            tasks[index - 1] += " - Completed"
-    return redirect(url_for('index'))
-
-@app.route("/delete", methods=["POST"])
-def delete_task():
-    tasks_to_delete = request.form.getlist("taskCheckbox")
-    tasks_to_delete.sort(reverse=True)
-    for index in map(int, complete_tasks):
-        if 1 <= index <= len(complete_tasks):
-            del tasks[index - 1]
-    return redirect(url_for('index'))
+@app.route("/delete<int:todo_id>", methods=["POST"])
+def delete_todo(todo_id):
+    global tasks
+    for task in tasks:
+        if task["id"] == todo_id:
+            tasks.remove(task)
+    return redirect(url_for("home"))
 
 if __name__ == "__main__":
     app.run(debug=True)
