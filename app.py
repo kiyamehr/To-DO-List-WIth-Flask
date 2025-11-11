825ee2041c6cee30104df29c82b7d4077541b78f
Kiamehr <kiamehrmoradii@gmail.com>
2025-11-11 12:02:27 +0330

    imporved CSS Visuals

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, request, redirect
-from random import randint
+import random
 
 app = Flask(__name__)
 
@@ -19,34 +19,37 @@ tasks = [
 def home():
     if request.method == "POST":
         todo_name = request.form["todo_name"]
-        cur_id = randint(1, 1000)
-        tasks.append({
-            "id" : cur_id,
-            "name" : todo_name,
-            "checked" : False,
-        })
+        cur_id = random.randint(1, 1000)
+        tasks.append(
+            {
+                "id": cur_id,
+                "name": todo_name,
+                "checked": False,
+            }
+        )
 
     return render_template("index.html", tasks=tasks)
+
 
 # <int:todo_id> = its gonna get an integer thats called todo_id \
 # so the checked is for that
 @app.route("/checked<int:todo_id>", methods=["POST"])
 def checked_todo(todo_id):
     for task in tasks:
-        if task['id'] == todo_id:
+        if task["id"] == todo_id:
             task["checked"] = not task["checked"]
             break
-    
+
     return redirect(url_for("home"))
-    
+
 
 @app.route("/delete<int:todo_id>", methods=["POST"])
 def delete_todo(todo_id):
-    global tasks
     for task in tasks:
         if task["id"] == todo_id:
             tasks.remove(task)
     return redirect(url_for("home"))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
